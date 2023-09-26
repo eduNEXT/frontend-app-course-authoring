66157f4019eb72f5c7dba315b8452b222a65f82c
Jhon Vente <jhon.vente@edunext.co>
2023-09-26 14:35:23 -0500

    feat: adding modules

---
+++ module.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  localModules: [
+    {
+      moduleName: "@edx/frontend-lib-content-components/dist",
+      dir: "./frontend-lib-content-components",
+      dist: "src",
+    },
+    {
+      moduleName: "@edx/frontend-lib-content-components",
+      dir: "./frontend-lib-content-components",
+      dist: "src",
+    },
+  ],
+};
