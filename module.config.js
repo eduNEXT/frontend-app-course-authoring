f478b617171b3ddbefa19baca8cac3477ca129a7
Jhon Vente <jhon.vente@edunext.co>
2023-09-26 20:17:09 -0500

    feat: separated frontend lib content

--- module.config.js
+++ module.config.js
@@ -2,12 +2,12 @@ module.exports = {
   localModules: [
     {
       moduleName: "@edx/frontend-lib-content-components/dist",
-      dir: "./frontend-lib-content-components",
+      dir: "./frontend-lib-test",
       dist: "src",
     },
     {
       moduleName: "@edx/frontend-lib-content-components",
-      dir: "./frontend-lib-content-components",
+      dir: "./frontend-lib-test",
       dist: "src",
     },
   ],
